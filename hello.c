e8c6d5311f96df2b96a55ef14ef1b2227a3233aa
techtanos <ftertanos@gmail.com>
2026-06-10 15:59:57 +0200

    started C programming - variables and printf

---
+++ hello.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+
+int main() {
+    printf("Hello, I am building flying cars.\n");
+    return 0;
+}
---
+++ variables.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int age = 20;
+    float payload = 2.5;
+    char name[] =  "Tertanos";
+    printf("Name: %s\n", name);
+    printf("Age: %d\n", age);
+    printf("Drone payload: %.1f kg\n", payload);
+
+    return 0;
+}
+
